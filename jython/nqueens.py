5b4eb15b3bb1bafcfd9454842eaba09ab294c3e0
athirabnair <anair71@gatech.edu>
2018-03-08 00:59:47 -0500

    new files

---
+++ jython/nqueens.py
@@ -0,0 +1,94 @@
+import random
+import time
+
+import opt.ga.NQueensFitnessFunction as NQueensFitnessFunction
+import dist.DiscreteDependencyTree as DiscreteDependencyTree
+import dist.DiscretePermutationDistribution as DiscretePermutationDistribution
+import dist.DiscreteUniformDistribution as DiscreteUniformDistribution
+import dist.Distribution as Distribution
+import opt.DiscreteChangeOneNeighbor as DiscreteChangeOneNeighbor
+import opt.EvaluationFunction as EvaluationFunction
+import opt.GenericHillClimbingProblem as GenericHillClimbingProblem
+import opt.HillClimbingProblem as HillClimbingProblem
+import opt.NeighborFunction as NeighborFunction
+import opt.RandomizedHillClimbing as RandomizedHillClimbing
+import opt.SimulatedAnnealing as SimulatedAnnealing
+import opt.SwapNeighbor as SwapNeighbor
+from opt.example import *
+import opt.ga.CrossoverFunction as CrossoverFunction
+import opt.ga.DiscreteChangeOneMutation as DiscreteChangeOneMutation
+import opt.ga.SingleCrossOver as SingleCrossOver
+import opt.ga.GenericGeneticAlgorithmProblem as GenericGeneticAlgorithmProblem
+import opt.ga.GeneticAlgorithmProblem as GeneticAlgorithmProblem
+import opt.ga.MutationFunction as MutationFunction
+import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
+import opt.ga.SwapMutation as SwapMutation
+import opt.prob.GenericProbabilisticOptimizationProblem as GenericProbabilisticOptimizationProblem
+import opt.prob.MIMIC as MIMIC
+import opt.prob.ProbabilisticOptimizationProblem as ProbabilisticOptimizationProblem
+import shared.FixedIterationTrainer as FixedIterationTrainer
+
+class NQueensTest:
+
+    def __init__(self, n = 10):
+        self.N = n
+
+    def main(self):
+        ranges = [random.randint(1,self.N) for i in range(self.N)]
+        ef = NQueensFitnessFunction()
+        odd = DiscretePermutationDistribution(self.N)
+        nf = SwapNeighbor()
+        mf = SwapMutation()
+        cf = SingleCrossOver()
+        df = DiscreteDependencyTree(.1)
+        hcp = GenericHillClimbingProblem(ef, odd, nf)
+        gap = GenericGeneticAlgorithmProblem(ef, odd, mf, cf)
+        pop = GenericProbabilisticOptimizationProblem(ef, odd, df)
+
+        rhc = RandomizedHillClimbing(hcp)
+        fit = FixedIterationTrainer(rhc, 100)
+        fit.train()
+        starttime = time.time()
+        print "RHC: %f" % ef.value(rhc.getOptimal())
+        print "RHC: Board Position: "
+        # print(ef.boardPositions())
+        print "Time : %0.03f" % (time.time() - starttime)
+
+        print("============================")
+
+        sa = SimulatedAnnealing(1E1, .1, hcp)
+        fit = FixedIterationTrainer(sa, 100)
+        fit.train()
+
+        starttime = time.time()
+        print "SA: %f" % ef.value(sa.getOptimal())
+        print("SA: Board Position: ")
+        # print(ef.boardPositions())
+        print "Time : %0.03f" % (time.time() - starttime)
+
+        print("============================")
+
+        starttime = time.time()
+        ga = StandardGeneticAlgorithm(200, 0, 10, gap)
+        fit = FixedIterationTrainer(ga, 100)
+        fit.train()
+        print "GA: %f" % ef.value(ga.getOptimal())
+        print("GA: Board Position: ")
+        # print(ef.boardPositions())
+        print "Time : %0.03f" % (time.time() - starttime)
+
+        print("============================")
+
+        starttime = time.time()
+        mimic = MIMIC(200, 10, pop)
+        fit = FixedIterationTrainer(mimic, 5)
+        fit.train()
+        print "MIMIC: %f" % ef.value(mimic.getOptimal())
+        print("MIMIC: Board Position: ")
+        # print(ef.boardPositions())
+        print "Time : %0.03f" % (time.time() - starttime)
+
+
+if __name__ == "__main__":
+    nqueens = NQueensTest(8)
+    nqueens.main()
--- jython/wine_test.py
+++ jython/wine_test.py
@@ -1,9 +1,10 @@
 """
 Implementation of randomized hill climbing, simulated annealing, and genetic algorithm to
-find optimal weights to a neural network that is classifying abalone as having either fewer
-or more than 15 rings.
+find optimal weights to a neural network that is classifying wine as either good or bad quality
 
-Based on AbaloneTest.java by Hannah Lau
+Based on AbaloneTest.java
+
+-Athira Nair
 """
 
 from __future__ import with_statement
@@ -11,6 +12,8 @@ from __future__ import with_statement
 import os
 import csv
 import time
+from operator import itemgetter
+import random
 
 from func.nn.backprop import BackPropagationNetworkFactory
 from shared import SumOfSquaresError, DataSet, Instance
@@ -21,25 +24,35 @@ import opt.SimulatedAnnealing as SimulatedAnnealing
 import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
 
 
-INPUT_FILE = os.path.join("..", "src", "opt", "test", "abalone.txt")
+INPUT_FILE = os.path.join("..", "..", "..", "Data", "winequality-white-modified.csv")
 
-INPUT_LAYER = 7
-HIDDEN_LAYER = 5
+INPUT_LAYER = 11
+HIDDEN_LAYER = 50
 OUTPUT_LAYER = 1
-TRAINING_ITERATIONS = 1000
+#TRAINING_ITERATIONS = 100
+TRAINING_ITERATIONS = {'RHC':1500, 'SA': 2500, 'GA':500}
 
+"""
+Defaults:
+
+HIDDEN_LAYER = 9
+    oa.append(RandomizedHillClimbing(nnop[0]))
+    oa.append(SimulatedAnnealing(1E11, .95, nnop[1]))
+    oa.append(StandardGeneticAlgorithm(200, 100, 10, nnop[2]))
+
+"""
 
 def initialize_instances():
-    """Read the abalone.txt CSV data into a list of instances."""
+    """Read the winequality-white.csv CSV data into a list of instances."""
     instances = []
 
     # Read in the abalone.txt CSV file
-    with open(INPUT_FILE, "r") as abalone:
-        reader = csv.reader(abalone)
+    with open(INPUT_FILE, "r") as wine:
+        reader = csv.reader(wine)
 
         for row in reader:
             instance = Instance([float(value) for value in row[:-1]])
-            instance.setLabel(Instance(0 if float(row[-1]) < 15 else 1))
+            instance.setLabel(Instance(0 if float(row[-1]) < 0.5 else 1))
             instances.append(instance)
 
     return instances
@@ -55,8 +68,8 @@ def train(oa, network, oaName, instances, measure):
     :param AbstractErrorMeasure measure:
     """
     print "\nError results for %s\n---------------------------" % (oaName,)
-
-    for iteration in xrange(TRAINING_ITERATIONS):
+    error_array = []
+    for iteration in xrange(TRAINING_ITERATIONS[oaName]):
         oa.train()
 
         error = 0.00
@@ -69,21 +82,34 @@ def train(oa, network, oaName, instances, measure):
             example = Instance(output_values, Instance(output_values.get(0)))
             error += measure.value(output, example)
 
-        print "%0.03f" % error
+        print "%d %0.03f" % (iteration,error)
+        error_array.append([iteration+1, round(error,3)])
+
+    filename = 'data/wine/error_' + oaName + '_' + str(TRAINING_ITERATIONS[oaName]) + '.csv'
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(error_array)
+    print 'wrote to file {}'.format(filename)
 
 
 def main():
-    """Run algorithms on the abalone dataset."""
+    """Run algorithms on the wine dataset."""
     instances = initialize_instances()
+    training_indices = random.sample(xrange(len(instances)), int(0.7 * len(instances)))
+    training_instances = itemgetter(*training_indices)(instances)
+    test_indices = list(set(range(len(instances))) - set(training_indices))
+    test_instances = itemgetter(*test_indices)(instances)
     factory = BackPropagationNetworkFactory()
     measure = SumOfSquaresError()
-    data_set = DataSet(instances)
+    data_set = DataSet(training_instances)
 
     networks = []  # BackPropagationNetwork
     nnop = []  # NeuralNetworkOptimizationProblem
     oa = []  # OptimizationAlgorithm
     oa_names = ["RHC", "SA", "GA"]
     results = ""
+    # result_array = [["Algorithm", "# Correctly Classified", "# Incorrectly Classified", "Training Time", "Testing Time", "Number of Iterations"]]
+    result_array = []
 
     for name in oa_names:
         classification_network = factory.createClassificationNetwork([INPUT_LAYER, HIDDEN_LAYER, OUTPUT_LAYER])
@@ -99,7 +125,7 @@ def main():
         correct = 0
         incorrect = 0
 
-        train(oa[i], networks[i], oa_names[i], instances, measure)
+        train(oa[i], networks[i], oa_names[i], training_instances, measure)
         end = time.time()
         training_time = end - start
 
@@ -107,7 +133,7 @@ def main():
         networks[i].setWeights(optimal_instance.getData())
 
         start = time.time()
-        for instance in instances:
+        for instance in test_instances:
             networks[i].setInputValues(instance.getData())
             networks[i].run()
 
@@ -125,10 +151,15 @@ def main():
         results += "\nResults for %s: \nCorrectly classified %d instances." % (name, correct)
         results += "\nIncorrectly classified %d instances.\nPercent correctly classified: %0.03f%%" % (incorrect, float(correct)/(correct+incorrect)*100.0)
         results += "\nTraining time: %0.03f seconds" % (training_time,)
-        results += "\nTesting time: %0.03f seconds\n" % (testing_time,)
-
+        results += "\nTesting time: %0.03f seconds" % (testing_time,)
+        results += "\nNumber of iterations: %d\n" % (TRAINING_ITERATIONS[oa_names[i]])
+        result_array.append([name, correct, incorrect, round(float(correct)/(correct+incorrect)*100.0,3), round(training_time,3), round(testing_time,3), TRAINING_ITERATIONS[oa_names[i]]])
     print results
 
+
+    with open("data/wine/test_results.csv","a") as result_file:
+        writer = csv.writer(result_file)
+        writer.writerows(result_array)
 
 if __name__ == "__main__":
     main()
