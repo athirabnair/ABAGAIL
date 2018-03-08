3f71912231cd7dcea1ada87a10bb0b43d584aec9
athirabnair <anair71@gatech.edu>
2018-03-07 23:33:06 -0500

    some data from tests & modifications

--- jython/wine_test.py
+++ jython/wine_test.py
@@ -3,6 +3,8 @@ Implementation of randomized hill climbing, simulated annealing, and genetic alg
 find optimal weights to a neural network that is classifying wine as either good or bad quality
 
 Based on AbaloneTest.java
+
+-Athira Nair
 """
 
 from __future__ import with_statement
@@ -10,6 +12,8 @@ from __future__ import with_statement
 import os
 import csv
 import time
+from operator import itemgetter
+import random
 
 from func.nn.backprop import BackPropagationNetworkFactory
 from shared import SumOfSquaresError, DataSet, Instance
@@ -26,8 +30,17 @@ INPUT_LAYER = 11
 HIDDEN_LAYER = 9
 OUTPUT_LAYER = 1
 #TRAINING_ITERATIONS = 100
-TRAINING_ITERATIONS = {'RHC':100, 'SA': 100, 'GA':100}
+TRAINING_ITERATIONS = {'RHC':2000, 'SA': 2000, 'GA':1000}
 
+"""
+Defaults:
+
+HIDDEN_LAYER = 9
+    oa.append(RandomizedHillClimbing(nnop[0]))
+    oa.append(SimulatedAnnealing(1E11, .95, nnop[1]))
+    oa.append(StandardGeneticAlgorithm(200, 100, 10, nnop[2]))
+
+"""
 
 def initialize_instances():
     """Read the winequality-white.csv CSV data into a list of instances."""
@@ -69,10 +82,10 @@ def train(oa, network, oaName, instances, measure):
             example = Instance(output_values, Instance(output_values.get(0)))
             error += measure.value(output, example)
 
-        print "%0.03f" % error
+        print "%d %0.03f" % (iteration,error)
         error_array.append([iteration+1, round(error,3)])
 
-    filename = 'data/wine/error_' + oaName + '_1.csv'
+    filename = 'data/wine/error_' + oaName + '_' + str(TRAINING_ITERATIONS[oaName]) + '.csv'
     with open(filename, 'w') as f:
         writer = csv.writer(f)
         writer.writerows(error_array)
@@ -80,11 +93,15 @@ def train(oa, network, oaName, instances, measure):
 
 
 def main():
-    """Run algorithms on the abalone dataset."""
+    """Run algorithms on the wine dataset."""
     instances = initialize_instances()
+    training_indices = random.sample(xrange(len(instances)), int(0.8 * len(instances)))
+    training_instances = itemgetter(*training_indices)(instances)
+    test_indices = list(set(range(len(instances))) - set(training_indices))
+    test_instances = itemgetter(*test_indices)(instances)
     factory = BackPropagationNetworkFactory()
     measure = SumOfSquaresError()
-    data_set = DataSet(instances)
+    data_set = DataSet(training_instances)
 
     networks = []  # BackPropagationNetwork
     nnop = []  # NeuralNetworkOptimizationProblem
@@ -108,7 +125,7 @@ def main():
         correct = 0
         incorrect = 0
 
-        train(oa[i], networks[i], oa_names[i], instances, measure)
+        train(oa[i], networks[i], oa_names[i], training_instances, measure)
         end = time.time()
         training_time = end - start
 
@@ -116,7 +133,7 @@ def main():
         networks[i].setWeights(optimal_instance.getData())
 
         start = time.time()
-        for instance in instances:
+        for instance in test_instances:
             networks[i].setInputValues(instance.getData())
             networks[i].run()
 
@@ -134,13 +151,13 @@ def main():
         results += "\nResults for %s: \nCorrectly classified %d instances." % (name, correct)
         results += "\nIncorrectly classified %d instances.\nPercent correctly classified: %0.03f%%" % (incorrect, float(correct)/(correct+incorrect)*100.0)
         results += "\nTraining time: %0.03f seconds" % (training_time,)
-        results += "\nTesting time: %0.03f seconds\n" % (testing_time,)
+        results += "\nTesting time: %0.03f seconds" % (testing_time,)
         results += "\nNumber of iterations: %d\n" % (TRAINING_ITERATIONS[oa_names[i]])
         result_array.append([name, correct, incorrect, float(correct)/(correct+incorrect)*100.0, round(training_time,3), round(testing_time,3), TRAINING_ITERATIONS[oa_names[i]]])
     print results
 
 
-    with open("test_results.txt","a") as result_file:
+    with open("data/wine/test_results.csv","a") as result_file:
         writer = csv.writer(result_file)
         writer.writerows(result_array)
 
