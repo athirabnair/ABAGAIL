9f3e88acff5e90e8a5be3039cc11c3a98e7851d3
athirabnair <anair71@gatech.edu>
2018-03-10 01:11:34 -0500

    plotting

--- jython/problems_plot.py
+++ jython/problems_plot.py
@@ -1,1 +1,22 @@
-import pandas as py
+import pandas as pd
+import matplotlib.pyplot as plt
+
+nqueens = pd.read_csv('data/wine/nqueens_results.csv')
+nqueens.drop(columns="Unnamed: 9", inplace=True)
+
+# fig, ax = plt.subplots()
+
+input_variation = nqueens[['Algorithm','N','Fitness Score']][nqueens['N']==120]
+# input_variation.set_index('Algorithm',inplace=True)
+ax = input_variation[input_variation['Algorithm'] == 'GA'].plot(x='N', y='Fitness Score', kind='scatter', label='GA', c='red')
+input_variation[input_variation['Algorithm'] == 'RHC'].plot(x='N', y='Fitness Score', kind='scatter', label='RHC', ax = ax, c='g')
+input_variation[input_variation['Algorithm'] == 'SA'].plot(x='N', y='Fitness Score', kind='scatter', label='SA', ax = ax, c='blue')
+
+input_variation[input_variation['Algorithm'] == 'MIMIC'].plot(x='N', y='Fitness Score', kind='scatter', label='MIMIC', ax = ax, c='y')
+
+# for key, grp in nqueens.groupby(['Algorithm']):
+#     ax = grp.plot(ax=ax, kind='line', x='N', y='Fitness Score', label=key)
+# input_variation.plot()
+plt.xlabel('N=120')
+plt.legend(loc='best')
+plt.show()
